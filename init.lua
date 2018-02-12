951fac83b29957cd1eab81194a6139249d580939
Zythia <jesse@toasted.games>
2018-02-11 20:26:57 -0800

    t push origin master initial commit

---
+++ init.lua
@@ -0,0 +1,53 @@
+minetest.register_node("craftlib:prismarite_ore",{
+	
+	description="Prismarite Ore",
+	tiles={name="default_stone.png^craftlib_prismarite_ore.png"},
+	groups={cracky=3,}
+	drop = {
+    items = {
+        {
+            items = {"craftlib:prismarite"},
+            rarity = 1
+        },
+        {
+            items = {"craftlib:prismarite"},
+            rarity = 2
+        },
+        {
+            items = {"craftlib:prismarite"},
+            rarity = 2
+        },
+        {
+            items = {"craftlib:prismarite"},
+            rarity = 2
+        },
+        {
+            items = {"craftlib:prismarite"},
+            rarity = 2
+        },
+	}
+	}
+})
+
+
+
+minetest.register_ore({
+	
+    ore_type = "scatter",
+    ore = "craftlib:prismarite_ore",
+    wherein = "default:stone",
+    clust_scarcity = 20 * 20 * 20,
+    clust_num_ores = 5,
+    clust_size = 3,
+    y_min = -31000,
+    y_max = -100,
+	
+
+})
+
+minetest.register_craftitem("craftlib:prismarite",{
+
+	description="Prismarite Shard",
+	inventory_image="craftlib_prismarite.png",
+
+	})
